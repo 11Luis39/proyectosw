829678671fb0d95d855b090b6416c8facbf87903
TonyTorres <soloalaaalaz@gmail.com>
2023-12-06 09:59:45 -0400

    mi tarea

--- src/index.js
+++ src/index.js
@@ -175,7 +175,8 @@ app.get('/get_contacts/:userPhone', async (req, res) => {
   
 
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  const PORT = 3000;
+  server.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+  });
+  
